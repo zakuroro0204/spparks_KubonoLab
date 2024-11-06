c5fcc66e41ac5b8cfd6a92cfc9a03f20455046e0
zakuroro0204 <ektaiga20001029@gmail.com>
2024-11-06 10:57:32 +0900

    Updating install.py

--- python/install.py
+++ python/install.py
@@ -1,67 +1,49 @@
-#!/usr/local/bin/python
+#!/usr/local/bin/python3 -i
+# preceeding line should have path for Python on your machine
 
-# copy SPPARKS src/libspparks.so and spparks.py to system dirs
+# demo.py
+# Purpose: illustrate use of many library interface commands
+# Syntax:  demo.py
+#          uses in.ising as SPPARKS input script
 
-instructions = """
-Syntax: python install.py [-h] [libdir] [pydir]
-        libdir = target dir for src/libspparks.so, default = /usr/local/lib
-        pydir = target dir for spparks.py, default = Python site-packages dir
-"""
+import sys
 
-import sys,os,commands
+# parse command line
 
-if (len(sys.argv) > 1 and sys.argv[1] == "-h") or len(sys.argv) > 3:
-  print instructions
-  sys.exit()
-
-if len(sys.argv) >= 2: libdir = sys.argv[1]
-else: libdir = "/usr/local/lib"
-
-if len(sys.argv) == 3: pydir = sys.argv[2]
-else: pydir = ""
-
-# copy C lib to libdir if it exists
-# warn if not in LD_LIBRARY_PATH or LD_LIBRARY_PATH is undefined
-
-if not os.path.isdir(libdir):
-  print "ERROR: libdir %s does not exist" % libdir
-  sys.exit()
-  
-if "LD_LIBRARY_PATH" not in os.environ:
-  print "WARNING: LD_LIBRARY_PATH undefined, cannot check libdir %s" % libdir
-else:
-  libpaths = os.environ['LD_LIBRARY_PATH'].split(':')
-  if libdir not in libpaths:
-    print "WARNING: libdir %s not in LD_LIBRARY_PATH" % libdir
-
-str = "cp ../src/libspparks.so %s" % libdir
-print str
-outstr = commands.getoutput(str)
-if len(outstr.strip()): print outstr
-
-# copy spparks.py to pydir if it exists
-# if pydir not specified, install in site-packages via distutils setup()
-
-if pydir:
-  if not os.path.isdir(pydir):
-    print "ERROR: pydir %s does not exist" % pydir
+argv = sys.argv
+if len(argv) != 1:
+    print("Syntax: demo.py")
     sys.exit()
-  str = "cp ../python/spparks.py %s" % pydir
-  print str
-  outstr = commands.getoutput(str)
-  if len(outstr.strip()): print outstr
-  sys.exit()
-  
-print "installing spparks.py in Python site-packages dir"
 
-os.chdir('../python')                # in case invoked via make in src dir
+me = 0
+# uncomment if running in parallel via Pypar
+#import pypar
+#me = pypar.rank()
+#nprocs = pypar.size()
 
-from distutils.core import setup
-sys.argv = ["setup.py","install"]    # as if had run "python setup.py install"
-setup(name = "spparks",
-      version = "6Mar13",
-      author = "Steve Plimpton",
-      author_email = "sjplimp@sandia.gov",
-      url = "http://spparks.sandia.gov",
-      description = "SPPARKS kinetic Monte Carlo library",
-      py_modules = ["spparks"])
+from spparks import spparks
+spk = spparks()
+
+# test out various library functions after running in.demo
+
+spk.file("in.ising")
+
+if me == 0:
+    print("\nPython output:")
+
+nglobal = spk.extract("nglobal", 0)
+nlocal = spk.extract("nlocal", 0)
+print("Nglobal, nlocal =", nglobal, nlocal)
+
+xyz = spk.extract("xyz", 5)
+print("Y coord of 100th lattice site =", xyz[99][1])
+
+eng = spk.energy()
+print("Energy of system =", eng)
+
+site = spk.extract("site", 1)
+print("Site values for 1,10,100 =", site[0], site[9], site[99])
+
+# uncomment if running in parallel via Pypar
+#print("Proc %d out of %d procs has" % (me,nprocs), spk)
+#pypar.finalize()
